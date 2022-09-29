7e83f0f79810625d1869cad4cbe216c208b09404
Franklin-tech01 <franklinmike2021@gmail.com>
2022-09-28 13:16:43 -1200

    added a new file

--- Alx C/Hello.c
+++ Alx C/Hello.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 
-    printf("hello world"\n);
+    printf("hello world\n");
 
     return 0;
 }
---
+++ Alx C/variableone.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* 
+* main - Entry point
+* description: this program talks about variables
+* Return (0);
+*/
+
+int main()
+{
+    /* Variables in C */
+    int myNumber = 10;
+    int mySecondNumber = 20;
+    int mythirdNumber = myNumber * mySecondNumber;
+    printf("the value of mythirdNUmber is %i", mythirdNumber);
+    return 0;
+}
