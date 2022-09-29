777c823babbdbaeb852fe253ce19c68fb617e32e
Franklin-tech01 <franklinmike2021@gmail.com>
2022-09-28 13:39:44 -1200

    variable continuation

---
+++ Alx C/variables2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{ 
+    /*this has to do with division of variables "we use the float keyword*/
+    float num1 = 10;
+    float num2 = 3;
+    float num3 = num1/num2;
+    printf("%.2 f", num3);
+    return 0;
+}
