79048ab18a674939a171f18aecea5b3cb0a94ce7
Franklin-tech01 <franklinmike2021@gmail.com>
2022-09-28 13:22:12 -1200

    added comments to variable code

--- Alx C/variableone.c
+++ Alx C/variableone.c
@@ -10,7 +10,9 @@ int main()
     /* Variables in C */
     int myNumber = 10;
     int mySecondNumber = 20;
+    /* this variable makes the addintion oof the first and second variable possible */
     int mythirdNumber = myNumber * mySecondNumber;
+    /*prints out the addition of the first and second number*/
     printf("the value of mythirdNUmber is %i", mythirdNumber);
     return 0;
 }
